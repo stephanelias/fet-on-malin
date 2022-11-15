92020edace855378de767d8b1f39f3820e93bf6c
Rudy <stephane.elias229@gmail.com>
2022-11-15 15:27:52 +0100

    Commit Gallery section + navbar fixed

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+window.onload = function() {
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    console.log(sticky)
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+        console.log(window.pageYOffset) ; 
+    }
+}
 
